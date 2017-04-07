250210790f79d5ca97b32e597fb189b366bb4b0e
lightertu <ruitu.login@gmail.com>
2017-04-06 20:04:24 -0700

    experienmenting with different uis

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,28 +5,39 @@ import IconButton from 'material-ui/IconButton';
 import NavigationClose from 'material-ui/svg-icons/navigation/close';
 import FlatButton from 'material-ui/FlatButton';
 
+import SideMenu from './SideMenu'
 import './Header.scss'
 
 function handleTouchTap() {
     alert('onTouchTap triggered on the title component');
 }
 
-const Header = () => (
-    <div>
-        <AppBar
-            title="Title"
-            iconClassNameRight="muidocs-icon-navigation-expand-more"
-            iconElementRight={
-                <IndexLink to='/' activeClassName='route--active'>
-                    <FlatButton label="Home" />
-                </IndexLink>
-            }
-        />
-        <Link to='/counter' activeClassName='route--active'>
-            Counter
-        </Link>
-    </div>
-);
+class Header extends  React.Component {
+    constructor() {
+        super();
+        this.state = { sideMenuOpen: false };
+    }
+
+    sideMenuToggle = () => {
+        this.setState( { sideMenuOpen: !this.state.sideMenuOpen } );
+    }
+
+    render() {
+        return (
+            <div>
+                <AppBar
+                    title="Team Divider"
+                    iconClassNameRight="muidocs-icon-navigation-expand-more"
+                    onLeftIconButtonTouchTap={ this.sideMenuToggle }
+                />
+                <Link to='/counter' activeClassName='route--active'>
+                    Counter
+                </Link>
+                <SideMenu open = { this.state.sideMenuOpen } toggle = { this.sideMenuToggle } />
+            </div>
+        )
+    }
+}
 
 /*
 export const Header = () => (
