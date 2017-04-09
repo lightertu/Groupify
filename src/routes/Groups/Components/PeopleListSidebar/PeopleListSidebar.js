603649350578d432ef9b2672390f83a758c37cb7
lightertu <ruitu.login@gmail.com>
2017-04-08 17:55:34 -0700

    solved the sidebar problem

--- src/routes/Groups/Components/PeopleListSidebar/PeopleListSidebar.js
+++ src/routes/Groups/Components/PeopleListSidebar/PeopleListSidebar.js
@@ -14,29 +14,13 @@ class PeopleListSidebar extends Component {
         return (
             <div>
                 <Button onClick={this.toggleVisibility}>Toggle Visibility</Button>
-                <Sidebar.Pushable as={Grid}>
+                <Sidebar.Pushable>
                     <Sidebar
-                        as={Menu}
                         animation='scale down'
                         width='thin'
-                        direction='right'
+                        direction='left'
                         visible={visible}
-                        icon='labeled'
-                        vertical
-                        inverted
                     >
-                        <Menu.Item name='home'>
-                            <Icon name='home' />
-                            Home
-                        </Menu.Item>
-                        <Menu.Item name='gamepad'>
-                            <Icon name='gamepad' />
-                            Games
-                        </Menu.Item>
-                        <Menu.Item name='camera'>
-                            <Icon name='camera' />
-                            Channels
-                        </Menu.Item>
                     </Sidebar>
                     <Sidebar.Pusher>
                         { this.props.groups }
@@ -47,5 +31,28 @@ class PeopleListSidebar extends Component {
     }
 }
 
+/*
+<List selection divided>
+    <List.Item>
+        <Image avatar src='https://react.semantic-ui.com/assets/images/avatar/small/helen.jpg' />
+        <List.Content>
+            <List.Header>Helen</List.Header>
+        </List.Content>
+    </List.Item>
+    <List.Item>
+        <Image avatar src='https://react.semantic-ui.com/assets/images/avatar/small/christian.jpg' />
+        <List.Content>
+            <List.Header>Christian</List.Header>
+        </List.Content>
+    </List.Item>
+    <List.Item>
+        <Image avatar src='https://react.semantic-ui.com/assets/images/avatar/small/daniel.jpg' />
+        <List.Content>
+            <List.Header>Daniel</List.Header>
+        </List.Content>
+    </List.Item>
+</List>
+*/
 export default PeopleListSidebar
+
 
