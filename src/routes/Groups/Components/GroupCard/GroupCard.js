cb243feb8033646dcc283d24393adb2aa0de3e0c
Lightertu <ruit@uoregon.edu>
2017-04-09 17:12:38 -0700

    made header element stiky and modularized the group page element

--- src/routes/Groups/Components/GroupCard/GroupCard.js
+++ src/routes/Groups/Components/GroupCard/GroupCard.js
@@ -2,7 +2,7 @@
  * Created by rui on 4/8/17.
  */
 import React from 'react'
-import {Icon, Card, Label, Segment, Item, Grid, Image} from 'semantic-ui-react'
+import {Icon, Card, Label, Segment, } from 'semantic-ui-react'
 
 class GroupCard extends React.Component {
     constructor() {
@@ -11,38 +11,29 @@ class GroupCard extends React.Component {
     }
 
     render() {
-        // some fake test date
-        const member = [
-            {
-                image: "http://react.semantic-ui.com/assets/images/wireframe/image.png"
-            },
-            {
-                image: "http://react.semantic-ui.com/assets/images/wireframe/image.png"
-            },
-            {
-                image: "http://react.semantic-ui.com/assets/images/wireframe/image.png"
-            },
-            {
-                image: "http://react.semantic-ui.com/assets/images/wireframe/image.png"
-            },
-        ];
-        // TODO: write a function to generate different row arrangements
+        const itemsInRow = 8;
         return (
-
             <Segment color='yellow' raised padded={ true } size="large">
-                <Label attached='top left'>Group 1</Label>
-                <Card.Group itemsPerRow={ 8 } stackable>
+                <Label attached='top left'> Group { this.props.groupNumber }</Label>
+                <Card.Group itemsPerRow={ itemsInRow } stackable>
                     {
-                        member.map((member) =>
-                            <Card image = { member.image }
-                            />
+                        this.props.members.map((member) =>
+                            <Card image = { member.image } />
                         )
                     }
                 </Card.Group>
-                <Label attached="top right"> <Icon name='user'/> 3/3 </Label>
+                <Label attached="top right">
+                    <Icon name='user'/> { this.props.members.length}/{ this.props.capacity }
+                    </Label>
             </Segment>
         )
     }
+}
+
+GroupCard.propTypes = {
+    members: React.PropTypes.array.isRequired,
+    capacity: React.PropTypes.number.isRequired,
+    groupNumber: React.PropTypes.number.isRequired
 }
 
 export default GroupCard;
--- src/routes/Groups/Components/GroupsView.js
+++ src/routes/Groups/Components/GroupsView.js
@@ -2,216 +2,155 @@
  * Created by rui on 4/7/17.
  */
 import React from 'react'
-import {Card, Feed} from 'semantic-ui-react'
-import {Icon, Label, Segment, Item, Grid, Image, List, Header} from 'semantic-ui-react'
+import {Icon, Label, Segment, Item, Grid, Image, List, Header, Menu, Button} from 'semantic-ui-react'
 import Sticky from 'react-stickynode'
 
 import "./GroupsView.scss"
 import GroupCard from "./GroupCard/GroupCard"
 
-const styles= {
-    position: "fixed",
-    height: "60%",
-    overflow: "scroll"
+const styles = {
+    //height: "60%",
+    //overflow: "scroll"
 }
 
-/*
-*/
-export const Groups = (props) => (
-    <div className="container">
-        <Grid>
-            <Grid.Row>
-                    <Grid.Column width = { 6 }>
-                        <Segment style={ styles }>
-                            <Header as='h3' textAlign='center'>
-                                <Header.Content>
-                                    People
-                                </Header.Content>
-                            </Header>
-                            <List divided verticalAlign='middle' size="massive" selection>
-                                <List.Item>
-                                    <Image avatar src='http://react.semantic-ui.com/assets/images/avatar/large/patrick.png' />
-                                    <List.Content>
-                                        <List.Header>Christian Rui</List.Header>
-                                    </List.Content>
-                                </List.Item>
-                                <List.Item>
-                                    <Image avatar src='http://react.semantic-ui.com/assets/images/avatar/large/patrick.png' />
-                                    <List.Content>
-                                        <List.Header>Daniel someone</List.Header>
-                                    </List.Content>
-                                </List.Item>
-                                <List.Item>
-                                    <Image avatar src='http://react.semantic-ui.com/assets/images/avatar/large/patrick.png' />
-                                    <List.Content>
-                                        <List.Header>Christian Rui</List.Header>
-                                    </List.Content>
-                                </List.Item>
-                                <List.Item>
-                                    <Image avatar src='http://react.semantic-ui.com/assets/images/avatar/large/patrick.png' />
-                                    <List.Content>
-                                        <List.Header>Daniel someone</List.Header>
-                                    </List.Content>
-                                </List.Item>
-                                <List.Item>
-                                    <Image avatar src='http://react.semantic-ui.com/assets/images/avatar/large/patrick.png' />
-                                    <List.Content>
-                                        <List.Header>Christian Rui</List.Header>
-                                    </List.Content>
-                                </List.Item>
-                                <List.Item>
-                                    <Image avatar src='http://react.semantic-ui.com/assets/images/avatar/large/patrick.png' />
-                                    <List.Content>
-                                        <List.Header>Daniel someone</List.Header>
-                                    </List.Content>
-                                </List.Item>
-                                <List.Item>
-                                    <Image avatar src='http://react.semantic-ui.com/assets/images/avatar/large/patrick.png' />
-                                    <List.Content>
-                                        <List.Header>Christian Rui</List.Header>
-                                    </List.Content>
-                                </List.Item>
-                                <List.Item>
-                                    <Image avatar src='http://react.semantic-ui.com/assets/images/avatar/large/patrick.png' />
-                                    <List.Content>
-                                        <List.Header>Daniel someone</List.Header>
-                                    </List.Content>
-                                </List.Item>
-                                <List.Item>
-                                    <Image avatar src='http://react.semantic-ui.com/assets/images/avatar/large/patrick.png' />
-                                    <List.Content>
-                                        <List.Header>Christian Rui</List.Header>
-                                    </List.Content>
-                                </List.Item>
-                                <List.Item>
-                                    <Image avatar src='http://react.semantic-ui.com/assets/images/avatar/large/patrick.png' />
-                                    <List.Content>
-                                        <List.Header>Daniel someone</List.Header>
-                                    </List.Content>
-                                </List.Item>
-                                <List.Item>
-                                    <Image avatar src='http://react.semantic-ui.com/assets/images/avatar/large/patrick.png' />
-                                    <List.Content>
-                                        <List.Header>Christian Rui</List.Header>
-                                    </List.Content>
-                                </List.Item>
-                                <List.Item>
-                                    <Image avatar src='http://react.semantic-ui.com/assets/images/avatar/large/patrick.png' />
-                                    <List.Content>
-                                        <List.Header>Daniel someone</List.Header>
-                                    </List.Content>
-                                </List.Item>
-                                <List.Item>
-                                    <Image avatar src='http://react.semantic-ui.com/assets/images/avatar/large/patrick.png' />
-                                    <List.Content>
-                                        <List.Header>Christian Rui</List.Header>
-                                    </List.Content>
-                                </List.Item>
-                                <List.Item>
-                                    <Image avatar src='http://react.semantic-ui.com/assets/images/avatar/large/patrick.png' />
-                                    <List.Content>
-                                        <List.Header>Daniel someone</List.Header>
-                                    </List.Content>
-                                </List.Item>
-                                <List.Item>
-                                    <Image avatar src='http://react.semantic-ui.com/assets/images/avatar/large/patrick.png' />
-                                    <List.Content>
-                                        <List.Header>Christian Rui</List.Header>
-                                    </List.Content>
-                                </List.Item>
-                                <List.Item>
-                                    <Image avatar src='http://react.semantic-ui.com/assets/images/avatar/large/patrick.png' />
-                                    <List.Content>
-                                        <List.Header>Daniel someone</List.Header>
-                                    </List.Content>
-                                </List.Item>
-                                <List.Item>
-                                    <Image avatar src='http://react.semantic-ui.com/assets/images/avatar/large/patrick.png' />
-                                    <List.Content>
-                                        <List.Header>Christian Rui</List.Header>
-                                    </List.Content>
-                                </List.Item>
-                                <List.Item>
-                                    <Image avatar src='http://react.semantic-ui.com/assets/images/avatar/large/patrick.png' />
-                                    <List.Content>
-                                        <List.Header>Daniel someone</List.Header>
-                                    </List.Content>
-                                </List.Item>
-                                <List.Item>
-                                    <Image avatar src='http://react.semantic-ui.com/assets/images/avatar/large/patrick.png' />
-                                    <List.Content>
-                                        <List.Header>Christian Rui</List.Header>
-                                    </List.Content>
-                                </List.Item>
-                                <List.Item>
-                                    <Image avatar src='http://react.semantic-ui.com/assets/images/avatar/large/patrick.png' />
-                                    <List.Content>
-                                        <List.Header>Daniel someone</List.Header>
-                                    </List.Content>
-                                </List.Item>
-                                <List.Item>
-                                    <Image avatar src='http://react.semantic-ui.com/assets/images/avatar/large/patrick.png' />
-                                    <List.Content>
-                                        <List.Header>Christian Rui</List.Header>
-                                    </List.Content>
-                                </List.Item>
-                                <List.Item>
-                                    <Image avatar src='http://react.semantic-ui.com/assets/images/avatar/large/patrick.png' />
-                                    <List.Content>
-                                        <List.Header>Daniel someone</List.Header>
-                                    </List.Content>
-                                </List.Item>
-                            </List>
-                        </Segment>
-                    </Grid.Column>
+class Person extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <List.Item>
+                <Image avatar src={ this.props.image }/>
+                <List.Content>
+                    <List.Header> { this.props.name } </List.Header>
+                </List.Content>
+                <List.Content floated="right" verticalAlign="middle">
+                    <Label color={ (this.props.isAssigned) ? "green" : "red" }>
+                        { (this.props.isAssigned) ? "assigned" : "unassigned"}
+                    </Label>
+                </List.Content>
+            </List.Item>
+        )
+    }
+}
 
-                <Grid.Column width={ 10 } >
-                    <GroupCard/>
-                    <GroupCard/>
-                    <GroupCard/>
-                    <GroupCard/>
-                    <GroupCard/>
-                    <GroupCard/>
-                    <GroupCard/>
-                    <GroupCard/>
-                    <GroupCard/>
-                    <GroupCard/>
-                    <GroupCard/>
-                    <GroupCard/>
-                    <GroupCard/>
-                    <GroupCard/>
-                    <GroupCard/>
-                    <GroupCard/>
-                    <GroupCard/>
-                    <GroupCard/>
-                    <GroupCard/>
-                    <GroupCard/>
-                    <GroupCard/>
-                    <GroupCard/>
-                    <GroupCard/>
-                    <GroupCard/>
-                    <GroupCard/>
-                    <GroupCard/>
-                    <GroupCard/>
-                    <GroupCard/>
-                    <GroupCard/>
-                    <GroupCard/>
-                    <GroupCard/>
-                    <GroupCard/>
-                </Grid.Column>
-            </Grid.Row>
-        </Grid>
-        </div>
-)
+Person.propTypes = {
+     isAssigned: React.PropTypes.bool.isRequired,
+     name: React.PropTypes.string.isRequired,
+     image: React.PropTypes.string.isRequired
+}
 
 
-Groups.propTypes = {
+const people =
+    [
+        {
+            name: "rui tu",
+            image: "http://react.semantic-ui.com/assets/images/avatar/small/joe.jpg",
+            isAssigned: true
+        },
+        {
+            name: "rui tu",
+            image: "http://react.semantic-ui.com/assets/images/avatar/small/joe.jpg",
+            isAssigned: true
+        },
+        {
+            name: "rui tu",
+            image: "http://react.semantic-ui.com/assets/images/avatar/small/joe.jpg",
+            isAssigned: true
+        },
+    ]
 
+const groups = [
+    {
+        groupNumber: 1,
+        capacity: 8,
+        members: people
+    },
+    {
+        groupNumber: 2,
+        capacity: 8,
+        members: people
+    },
+    {
+        groupNumber: 3,
+        capacity: 8,
+        members: people
+    },
+    {
+        groupNumber: 4,
+        capacity: 8,
+        members: people
+    },
+    {
+        groupNumber: 5,
+        capacity: 8,
+        members: people
+    },
+    {
+        groupNumber: 6,
+        capacity: 8,
+        members: people
+    }
+];
+
+export class GroupsView extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        const peopleListWidth = 6;
+        const groupCardsWidth = 10;
+        const peopleListTopPadding = 110;
+
+        return (
+            <div className="container">
+                <Grid>
+                    <Grid.Row>
+                        <Grid.Column width={ peopleListWidth }>
+                            <Sticky top={ peopleListTopPadding }>
+                                <Segment raised style={ styles } color='teal'>
+                                    <Label attached='top'>People</Label>
+                                    <List verticalAlign='middle' size="small" selection>
+                                        {
+                                            people.map((personObj) => (
+                                                    <Person
+                                                        name = { personObj.name }
+                                                        isAssigned = { personObj.isAssigned }
+                                                        image = { personObj.image }
+                                                    />
+                                                )
+                                            )
+                                        }
+                                    </List>
+                                </Segment>
+                            </Sticky>
+                        </Grid.Column>
+
+                        <Grid.Column width={ groupCardsWidth }>
+                            {
+                                groups.map(
+                                    (group) => (
+                                        <GroupCard members={ group.members }
+                                                   capacity= { group.capacity }
+                                                   groupNumber= { group.groupNumber}/>
+                                    )
+                                )
+                            }
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </div>
+        )
+    }
+}
+
+
+GroupsView.propTypes = {
     /*
-     counter: React.PropTypes.number.isRequired,
-     doubleAsync: React.PropTypes.func.isRequired,
-     increment: React.PropTypes.func.isRequired
-     */
+    people: React.PropTypes.array.isRequired,
+    groups: React.PropTypes.array.isRequired
+    */
 }
 
-export default Groups
+export default GroupsView
