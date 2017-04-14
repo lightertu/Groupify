63e687cbd893840f105849ef8e70567fced9acb1
Rui Tu <ruit@uoregon.edu>
2017-04-13 22:45:54 -0700

    Revert "Popup"

--- src/routes/Groups/Components/GroupCard/GroupCard.js
+++ src/routes/Groups/Components/GroupCard/GroupCard.js
@@ -2,9 +2,7 @@
  * Created by rui on 4/8/17.
  */
 import React from 'react'
-import PropTypes from 'prop-types';
-import { Icon, Card, Label, Segment, Image } from 'semantic-ui-react'
-import MemberProfilePopup from "../MemberProfilePopup";
+import {Icon, Card, Label, Segment, Popup, Image } from 'semantic-ui-react'
 
 class GroupCard extends React.Component {
     constructor() {
@@ -12,62 +10,66 @@ class GroupCard extends React.Component {
         this.state = {activeItem: 'home'}
     }
 
-    render() {
-        let generateEmptySpots = ()  => {
-            let emptyNum = this.props.capacity - this.props.members.length;
-            let result = [ ];
-            for (let i = 0; i < emptyNum; i++) {
-                result.push(
-                    <Card image="https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Transparent_square.svg/2000px-Transparent_square.svg.png"/>
-                )
-            }
-            return result;
-        };
-        let pickLabelColor = (size, capacity) => {
-            if (size === capacity)
-                return "green";
-            else if (size > capacity)
-                return "red";
-            else
-                return "";
-        };
-        let generateMemberPictures = (members) => (
-            members.map((member) =>
-                <Card>
-                    <MemberProfilePopup trigger = { <Image src = { member.image } /> }
-                                        position ="top right"
-                                        offset = { 0 }
-                                        name = { member.name }
-                                        image = { member.image }
-                                        groupNumber = {member.groupNumber }
-                                        skills = { member.skills }
-                                        availability = { member.availability }
-                    />
-                </Card>
+    insertEmptySpots = ()  => {
+        let emptyNum = this.props.capacity - this.props.members.length;
+        let result = [ ];
+        for (let i = 0; i < emptyNum; i++) {
+            result.push(
+                <Card image="http://www.iconarchive.com/download/i93787/custom-icon-design/silky-line-user/user2-add.ico"/>
             )
-        );
+        }
+        return result;
+    }
 
+
+    labelColor = () => {
+        if (this.props.members.length === this.props.capacity) {
+            return "green";
+        } else if (this.props.members.length > this.props.capacity) {
+            return "red";
+        } else {
+            return "grey";
+        }
+    }
+
+    render() {
+        const itemsInRow = 5;
         return (
             <Segment color='yellow' raised padded={ true } size="large">
                 <Label attached='top left'> Group { this.props.groupNumber }</Label>
-                <Card.Group itemsPerRow={ this.props.itemsPerRow} stackable>
-                    { generateMemberPictures(this.props.members) }
-                    { generateEmptySpots() }
+                <Card.Group itemsPerRow={ itemsInRow } stackable>
+
+                    {
+                        this.props.members.map((member) =>
+                        <Card>
+                            <Popup
+                                key={ member.name }
+                                trigger= { <Image src = { member.image } /> }
+                                header={ member.name }
+                                content={ "Good stuff" }
+                            >
+                            </Popup>
+                        </Card>
+                        )
+                    }
+
+                    {
+                        this.insertEmptySpots()
+                    }
+
                 </Card.Group>
-                <Label color = { pickLabelColor(this.props.members.length, this.props.capacity) }
-                       attached="top right">
-                    <Icon name='user'/> { this.props.members.length } / { this.props.capacity }
-                </Label>
+                <Label color = {this.labelColor()} attached="top right">
+                    <Icon name='user'/> { this.props.members.length}/{ this.props.capacity }
+                    </Label>
             </Segment>
         )
     }
 }
 
 GroupCard.propTypes = {
-    members: PropTypes.array.isRequired,
-    capacity: PropTypes.number.isRequired,
-    groupNumber: PropTypes.number.isRequired,
-    itemsPerRow: PropTypes.number.isRequired
-};
+    members: React.PropTypes.array.isRequired,
+    capacity: React.PropTypes.number.isRequired,
+    groupNumber: React.PropTypes.number.isRequired
+}
 
 export default GroupCard;
--- src/routes/Groups/Components/PeopleListSidebar/PeopleListSidebar.js
+++ src/routes/Groups/Components/PeopleListSidebar/PeopleListSidebar.js
@@ -6,43 +6,36 @@
  */
 
 import React from 'react'
-import {Segment, Image, List, Icon, Button, Header} from 'semantic-ui-react'
+import { Label, Segment, Image, List, Icon, Popup } from 'semantic-ui-react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import {Drawer} from "material-ui"
-import PropTypes from "prop-types"
-import MemberProfilePopup from "../MemberProfilePopup";
+import { Drawer } from "material-ui"
 
 class Person extends React.Component {
     constructor(props) {
         super(props)
     }
-
     render() {
         return (
-            <MemberProfilePopup
-                name={ this.props.name }
-                image={ this.props.image }
-                groupNumber={ this.props.groupNumber }
-                skills={ this.props.skills }
-                availability={ this.props.availability }
-                position="right center"
-                offset={ 20 }
-                hoverable
-                trigger={
-                    <List.Item>
-                        <Image size="mini" shape="rounded" verticalAlign="middle" src={ this.props.image }/>
-                        <List.Content>
-                            <List.Header> { this.props.name } </List.Header>
-                        </List.Content>
-                        <List.Content floated="right" verticalAlign="middle" style={ {paddingTop: "5%"}}>
-                            { (this.props.groupNumber >= 0) ? <Icon name="check" color="green"/> : <div>&nbsp;</div>}
-                        </List.Content>
-                    </List.Item>
-                }
-            />
+
+        <Popup
+          trigger={
+            <List.Item>
+              <Image size="mini" shape="rounded" verticalAlign="middle" src={ this.props.image }/>
+              <List.Content>
+                <List.Header> { this.props.name } </List.Header>
+              </List.Content>
+              <List.Content floated="right" verticalAlign="middle" style= { {paddingTop: "5%"}}>
+                { (this.props.groupNumber > 0) ? <Icon name="check" color="green"/> : <div>&nbsp;</div>}
+              </List.Content>
+            </List.Item>
+          }
+          content='I am positioned to the top left'
+          position='right center'
+        />
         )
     }
 }
+
 
 const peopleListStyle = {
     paddingTop: "23%"
@@ -54,60 +47,27 @@ class PeopleListSidebar extends React.Component {
     }
 
     render() {
-
-        let getUngroupedNumber = (members) => (
-            members.filter((personObj) => (
-                personObj.groupNumber < 0
-            )).length
-        );
-
-        let generateSidebarList = (members) => (
-            <List verticalAlign='middle' size="small" selection animated>
-                {
-                    members.filter((personObj) => (
-                        personObj.groupNumber < 0
-                    ))
-
-                    .map((personObj) => (
-                        <Person
-                            name={ personObj.name }
-                            image={ personObj.image }
-                            groupNumber={ personObj.groupNumber }
-                            skills={ personObj.skills }
-                            availability={ personObj.availability }
-                        />
-                    ))
-                }
-            </List>
-        );
-
-        let generateEmailButtom = () => (
-            <div style={ {paddingTop: "200%", textAlign: "center"} }>
-                <Header as='h2'>
-                    All Grouped!
-                    <Header.Subheader>
-                        Next step is to notify all the students
-                    </Header.Subheader>
-                </Header>
-                <Button color='green'>Send Out Email</Button>
-            </div>
-        );
-
         return (
             <MuiThemeProvider>
                 <Drawer
                     docked={ true }
                     open={ true }
                     zDepth={ 1 }
-                    containerStyle={ {backgroundColor: "#F6F7F9"} }
                 >
-                    <div style={ peopleListStyle }>
+                    <div style = { peopleListStyle }>
                         <Segment basic>
-                            {
-                                (getUngroupedNumber(this.props.people)) ?
-                                    generateSidebarList(this.props.people) :
-                                    generateEmailButtom()
-                            }
+                            <List verticalAlign='middle' size="small" selection>
+                                {
+                                    this.props.people.map((personObj) => (
+                                            <Person
+                                                name = { personObj.name }
+                                                groupNumber = { personObj.groupNumber }
+                                                image = { personObj.image }
+                                            />
+                                        )
+                                    )
+                                }
+                            </List>
                         </Segment>
                     </div>
                 </Drawer>
@@ -116,18 +76,14 @@ class PeopleListSidebar extends React.Component {
     }
 }
 
-/*
- */
 Person.propTypes = {
-    name: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired,
-    groupNumber: PropTypes.number.isRequired,
-    availability: PropTypes.array.isRequired,
-    skills: PropTypes.array.isRequired,
+    groupNumber: React.PropTypes.number.isRequired,
+    name: React.PropTypes.string.isRequired,
+    image: React.PropTypes.string.isRequired
 };
 
 PeopleListSidebar.propTypes = {
-    people: PropTypes.array.isRequired
+    people: React.PropTypes.array.isRequired,
 };
 
 export default PeopleListSidebar
