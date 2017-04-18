8f723bf5cbe53aaf44fe8dc281064a751476b4dd
Joseph <j.d.livni@gmail.com>
2017-04-17 20:42:43 -0700

    progress on Dashboard

---
+++ src/routes/Dashboard/components/Dashboard.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import MenuSideBar from './MenuSideBar';
+import View from './Content';
+
+class Dashboard extends React.Component {
+    constructor() {
+        super();
+        this.state = {groups: true}
+    }
+
+    toggleView = () => this.setState({ groups: !this.state.groups })
+
+    render() {
+        let data = [];
+        for (let i = 0; i < 10; i++) {
+                let card = {};
+                card.color = "blue";
+                card.title = "test";
+                data.push(card);
+            }
+
+        return (
+            <div class="ui bottom attached segment pushable">
+                <MenuSideBar toggleView={this.toggleView.bind(this)}/>
+                <View data={data}/> 
+            </div>
+        )
+    }
+}
+
+export default Dashboard;
---
+++ src/routes/Dashboard/components/MenuSideBar.js
@@ -0,0 +1,59 @@
+import React from 'react'
+
+class MenuSideBar extends React.Component {
+    constructor() {
+        super();
+        this.state = {}
+    }
+
+    render() {
+        var menuStyle = {
+            paddingTop: 55
+        };
+
+        var menuContentStyle = {
+            paddingTop: 10,
+            paddingBottom: 10,
+            paddingLeft: 10,
+            paddingRight: 10,
+            textAlign: "center"
+        };
+
+        return (
+            <div>
+                <div className="ui visible left vertical sidebar menu" style={menuStyle} >
+        
+                <img className="ui centered small circular bordered image" src="https://semantic-ui.com/images/avatar2/large/matthew.png"/>
+                <div className="menu content" style={menuContentStyle}>
+                    <a className="header">Michael</a>
+                    <div className="meta">
+                      <span className="date">Joined in 2016</span>
+                    </div>
+                    <div className="description">
+
+                    </div>
+                </div>
+
+                <a className="item">
+                  <i className="group icon"></i>
+                  Groups
+                </a>
+                <a className="item">
+                  <i className="write icon"></i>
+                  Survey
+                </a>
+                <a className="item">
+                  <i className="cloud icon"></i>
+                  Data
+                </a>
+                <a className="item">
+                  <i className="calendar icon"></i>
+                  History
+                </a>
+              </div>
+            </div>
+        )
+    }
+}
+
+export default MenuSideBar;
