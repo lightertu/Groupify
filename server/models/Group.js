d48ed972b9bfaa5a167b53b2ebdd29014aa0e298
Joseph <j.d.livni@gmail.com>
2017-04-18 11:54:46 -0700

    start of api. working at localhost:3000/api/groups

---
+++ server/models/Group.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose')
+
+var GroupSchema = new mongoose.Schema({
+	form: {type:Number, default: -1},
+	students: {type:Array, default: []},
+	color: {type:String, default: 'blue'},
+	timestamp: {type:Date, default:Date.now},
+	title: {type:String, default: ''},
+	success: {type:Number, default: -1}
+})
+
+module.exports = mongoose.model('GroupSchema', GroupSchema)
+
