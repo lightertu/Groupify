9228e7c5bd5e1e436800ea259f624718dea012ac
lightertu <ruitu.login@gmail.com>
2017-04-19 00:42:01 -0700

    sidebar rerender working

--- src/routes/Groups/modules/reducer.js
+++ src/routes/Groups/modules/reducer.js
@@ -9,6 +9,29 @@ const initialState = {
     totalCapacity: 0
 };
 
+
+let arrayObjectIndexOf = (myArray, searchTerm, property) => {
+    for(let i = 0, len = myArray.length; i < len; i++) {
+        if (myArray[i][property] === searchTerm) {
+            return i;
+        }
+    }
+    return -1;
+};
+
+
+let handleUpdateParticipantGroupsNumber = (state, payload) => {
+    let newState = Object.assign({}, state),
+        participantId = payload.participantId,
+        oldGroupNumber = payload.oldGroupNumber,
+        newGroupNumber = payload.newGroupNumber,
+        participantIndex = arrayObjectIndexOf(newState.participants, participantId, "participantId");
+
+    //console.log(JSON.stringify(participantIndex, null, 2));
+    newState.participants[participantIndex].groupNumber = newGroupNumber;
+    return newState;
+};
+
 export default function groupsReducer (state = initialState, action) {
     switch(action.type) {
         case Actions.FETCH_PARTICIPANT_LIST:
@@ -25,7 +48,7 @@ export default function groupsReducer (state = initialState, action) {
             return state;
             break;
         case Actions.UPDATE_PARTICIPANT_GROUP_NUMBER:
-            // TODO: change it before server response
+            handleUpdateParticipantGroupsNumber(state, action.payload);
             return state;
         case Actions.UPDATE_PARTICIPANT_GROUP_NUMBER_SUCCESS:
             return state;
