d6feb31491004b3d945b664239c5e0ef4861a70b
Joseph <j.d.livni@gmail.com>
2017-04-20 21:30:29 -0700

    authorization now works

--- server/main.js
+++ server/main.js
@@ -38,26 +38,31 @@ var auth = function(req, res, next) {
 };
 
 // Login endpoint
+// Authentication and Authorization Middleware
+var auth = function(req, res, next) {
+  if (req.session && req.session.user === "amy" && req.session.admin)
+    return next();
+  else
+    return res.sendStatus(401);
+};
+ 
+// Login endpoint
 app.get('/login', function (req, res) {
-    if(!req.query.username || !req.query.password) {
-        res.send('login failed');
-    } else if(req.query.username === 'joseph' || req.query.password === 'password') {
-        req.session.user = 'joseph';
-        req.session.admin = true;
-        res.send('login success');
-    }
+  if (!req.query.username || !req.query.password) {
+    res.send('login failed');    
+  } else if(req.query.username === "amy" || req.query.password === "amyspassword") {
+    req.session.user = "amy";
+    req.session.admin = true;
+    res.send("login success!");
+  }
 });
-
-//logout endpoint
-app.get('/logout', function (req, res) {
+ 
+// Logout endpoint
+app.get('/logout', auth, function (req, res) {
   req.session.destroy();
   res.send("logout success!");
 });
-// Get content endpoint
-app.get('/content', auth, function (req, res) {
-    res.send("You can only see this after you've logged in.");
-});
-
+ 
 // Apply gzip compression
 app.use(compress())
 app.use('/api', api);
@@ -90,7 +95,7 @@ if (project.env === 'development') {
     // This rewrites all routes requests to the root /index.html file
     // (ignoring file requests). If you want to implement universal
     // rendering, you'll want to remove this middleware.
-    app.use('*', function (req, res, next) {
+    app.use('*', auth, function (req, res, next) {
         const filename = path.join(compiler.outputPath, 'index.html')
         compiler.outputFileSystem.readFile(filename, (err, result) => {
             if (err) {
