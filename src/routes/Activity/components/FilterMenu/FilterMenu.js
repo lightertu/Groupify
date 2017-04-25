25ad53ef3fbb5e90583b8e30fe76a1f1b9ace076
Lightertu <ruit@uoregon.edu>
2017-04-24 21:53:42 -0700

    filter menu style

--- src/routes/Activity/components/FilterMenu/FilterMenu.js
+++ src/routes/Activity/components/FilterMenu/FilterMenu.js
@@ -21,28 +21,32 @@ class FilterMenu extends React.Component {
 
     render() {
         const options = [
-            { key: 'skill', text: 'Java', value: 'Java' },
+            {key: 'skill', text: 'Java', value: 'Java'},
         ];
 
         const filterMenuStyle = {
             marginBottom: '4%',
-                zIndex: 0,
-                backgroundColor: '#F6F7F9',
-                paddingLeft: "30px",
-                borderRadius: "10px",
+            zIndex: 3000,
+            backgroundColor: '#F6F7F9',
+            paddingLeft: "30px",
+            borderRadius: "10px",
         };
 
         const afterStickedStyle = {
-            zIndex: 1700,
+            zIndex: 3000,
             marginTop: '54px',
             borderRadius: "4px",
             backgroundColor: '#454749',
         };
 
         const inputStyle = {
-            marginLeft: "-8%",
-            border: "0px !important",
-    };
+            marginLeft: "-11%",
+            //border: "0px !important",
+        };
+
+        const buttonStyle = {
+            width: "120px"
+        };
         return (
             <div>
                 <Sticky isActive={true} style={ filterMenuStyle } topOffset={-55} stickyStyle={ afterStickedStyle  }>
@@ -50,17 +54,45 @@ class FilterMenu extends React.Component {
                         <Menu secondary>
                             <Menu.Item fitted>
                                 <Input
-                                    label={<Dropdown options={options} defaultValue='Java' />}
                                     icon='search'
-                                    labelPosition={'right'}
                                     iconPosition='left'
-                                    placeholder='Search...'
+                                    placeholder='Search By Name...'
                                     size="big"
-                                    style = {inputStyle}
+                                    style={inputStyle}
+                                    disabled
                                 />
                             </Menu.Item>
+
+
+                            <Menu.Item fitted>
+                                <Dropdown text='Skill' icon='filter' floating labeled button className='icon' style={ {zIndex: 400000000} }>
+                                    <Dropdown.Menu>
+                                        <Dropdown.Header content='Search Issues'/>
+                                        <Input icon='search' iconPosition='left' name='search'/>
+                                        <Dropdown.Header icon='tags' content='Filter by tag'/>
+                                        <Dropdown.Divider />
+                                        <Dropdown.Item label={{color: 'red', empty: true, circular: true}}
+                                                       text='Important'/>
+                                    </Dropdown.Menu>
+                                </Dropdown>
+                            </Menu.Item>
+
+                            <Menu.Item fitted>
+                                <Dropdown text='Time' icon='filter' floating labeled button className='icon'>
+                                    <Dropdown.Menu>
+                                        <Dropdown.Header content='Search Issues'/>
+                                        <Input icon='search' iconPosition='left' name='search'/>
+                                        <Dropdown.Header icon='tags' content='Filter by tag'/>
+                                        <Dropdown.Divider />
+                                        <Dropdown.Item label={{color: 'red', empty: true, circular: true}}
+                                                       text='Important'/>
+                                    </Dropdown.Menu>
+                                </Dropdown>
+                            </Menu.Item>
+
                             <Menu.Menu position='right'>
-                                <Button icon='detective' content='Smart Group' size="big"/>
+                                <Button icon='detective' content='Smart' size="medium" color="green" style={ buttonStyle }/>
+                                <Button icon='shuffle' content='Random ' size="medium" color="yellow" style={ buttonStyle }/>
                             </Menu.Menu>
                         </Menu>
                     </Segment>
