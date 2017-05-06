059038ac852fe20fed28352b87a1e053221c61c2
Matt Almenshad <almensha@uoregon.edu>
2017-05-05 18:06:52 -0700

    Welcome routes

--- src/routes/Welcome/components/WelcomeView.js
+++ src/routes/Welcome/components/WelcomeView.js
@@ -1,9 +1,31 @@
-import React from 'react'
-import { Grid, Row, Col } from 'react-bootstrap';
-import  Welcome from "./Welcome"
+import React, { Component } from 'react'
+import { Button, Header } from 'semantic-ui-react'
 
-export const WelcomeView = (props) => (
-    <Welcome />
-)
+export default class WelcomeView extends Component {
+    constructor(props) {
+        super(props);
+    }
 
-export default WelcomeView
+    render() {
+
+        return (
+            <div style={{textAlign:'center', marginTop:'50%'}}>
+                    <Header size='huge'>
+                        Welcome to Groupify
+                    </Header>
+
+                    <Header size='large'>
+                        <i>The most efficient web application for creating teams
+                        based on timewise availability.</i>
+                    </Header>
+                    
+                    <a href="/dashboard">
+                        <Button style={{marginTop:20}} className="massive ui right labeled icon blue button">
+                            <i className="arrow right icon"></i>
+                            Get Started
+                        </Button>
+                   </a>
+           </div>
+        )
+    }
+}
