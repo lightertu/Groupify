777438ea5e408c5262e4c32291f42ea08bac8006
lightertu <ruitu.login@gmail.com>
2017-05-09 12:46:21 -0700

    init structure for reducers and handlers

--- src/routes/Dashboard/modules/reducer.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by rui on 5/2/17.
- */
-const initialState = 0;
-export default function dashboardReducer (state = initialState, action) {
-    return state;
-}
---
+++ src/routes/Dashboard/modules/reducer/index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by rui on 5/9/17.
+ */
+
+import reducer from "./reducer"
+
+export default reducer
