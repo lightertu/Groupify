46bf06c5f09067559facb0148ff04610eb76dd9b
lightertu <ruitu.login@gmail.com>
2017-05-09 13:29:19 -0700

    added more structure to activity and dashboard

---
+++ src/routes/Activity/modules/reducer/actionsHandlers/generateGroupAssignmentActionsHandlers.js
@@ -0,0 +1,19 @@
+/**
+ * Created by rui on 5/9/17.
+ */
+
+let handleGenerateGroupAssignment = (state, payload) => {
+    return state;
+};
+let handleGenerateGroupAssignmentSuccess = (state, payload) => {
+    return state;
+};
+let handleGenerateGroupAssignmentFailure = (state, payload) => {
+    return state;
+};
+
+export {
+    handleGenerateGroupAssignment,
+    handleGenerateGroupAssignmentSuccess,
+    handleGenerateGroupAssignmentFailure
+}
---
+++ src/routes/Activity/modules/reducer/actionsHandlers/updateParticipantGroupNumberActionsHandlers.js
@@ -0,0 +1,43 @@
+/**
+ * Created by rui on 5/9/17.
+ */
+
+let arrayObjectIndexOf = (myArray, searchTerm, property) => {
+    for(let i = 0, len = myArray.length; i < len; i++) {
+        if (myArray[i][property] === searchTerm) {
+            return i;
+        }
+    }
+    return -1;
+};
+
+let handleUpdateParticipantGroupsNumber = (state, payload) => {
+    let newState = (JSON.parse(JSON.stringify(state))),
+
+        participantId = payload.participantId,
+        oldGroupNumber = payload.oldGroupNumber,
+        newGroupNumber = payload.newGroupNumber,
+        participantIndex = arrayObjectIndexOf(newState.participants, participantId, "participantId");
+
+    console.log(JSON.stringify(participantIndex, null, 2));
+    newState.participants[participantIndex].groupNumber = newGroupNumber;
+    let temp = Object.assign({}, newState.participants[participantIndex]);
+    newState.participants.splice(participantIndex, 1);
+    newState.participants.push(temp);
+
+    return newState;
+};
+
+let handleUpdateParticipantGroupsNumberSuccess = (state, payload) => {
+    return state;
+};
+
+let handleUpdateParticipantGroupsNumberFailure = (state, payload) => {
+    return state;
+};
+
+export {
+    handleUpdateParticipantGroupsNumber,
+    handleUpdateParticipantGroupsNumberSuccess,
+    handleUpdateParticipantGroupsNumberFailure,
+}
