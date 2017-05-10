bc028cbbf60c1bb2e6caf2949bfb3bcdde014dc2
lightertu <ruit@uoregon.edy>
2017-05-10 12:27:43 -0700

    added another route

---
+++ server/routes/auth/resetpassword/controllers/resetpasswordControllers.js
@@ -0,0 +1,11 @@
+/**
+ * Created by rui on 5/9/17.
+ */
+
+const User = require("../../../../models/").User;
+
+module.exports = {
+    resetpasswordController: function(req, res) {
+        console.log("reset password");
+    },
+};
