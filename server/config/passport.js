bc35a397fe2b8c10d48847e2efb47f0563ef96c6
lightertu <ruitu.login@gmail.com>
2017-05-10 00:20:04 -0700

    more

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,8 +1,9 @@
 /**
  * Created by rui on 5/9/17.
  */
-const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
+const PassportJwt = require('passport-jwt');
+const JwtStrategy = PassportJwt.Strategy;
+const ExtractJwt = PassportJwt.ExtractJwt;
 const User = require('../models').User;
 let config = require('../config/main');
 
@@ -13,8 +14,7 @@ module.exports = function (passport) {
     opts.secretOrKey = config.secret;
 
     passport.use(new JwtStrategy(opts, function (jwt_payload, done) {
-        // need to know how does findone behaves it seems to be really buggy
-        // implement promise instead of call back
+        // TODO: need to know how does findone behaves it seems to be really buggy
         User.findOne({_id: jwt_payload._doc._id}).exec()
             .then(function (user) {
                 if (user) {
@@ -23,7 +23,7 @@ module.exports = function (passport) {
                     done(null, false);
                 }
             })
-            .catch(function(err){
+            .catch(function (err) {
                 if (err) {
                     return done(err, false);
                 }
