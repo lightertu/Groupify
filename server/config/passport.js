561ed5f738b6119668f3fbb7b809ee2b51706a06
lightertu <ruitu.login@gmail.com>
2017-05-10 00:07:50 -0700

    callback to promise

--- server/config/passport.js
+++ server/config/passport.js
@@ -7,21 +7,26 @@ const User = require('../models').User;
 let config = require('../config/main');
 
 // Setup work and export for the JWT passport strategy
-module.exports = function(passport) {
+module.exports = function (passport) {
     let opts = {};
     opts.jwtFromRequest = ExtractJwt.fromAuthHeader();
     opts.secretOrKey = config.secret;
-    passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
-        User.findOne({id: jwt_payload.id}, function(err, user) {
-            if (err) {
-                return done(err, false);
-            }
-            if (user) {
-                console.log(jwt_payload.id);
-                done(null, user);
-            } else {
-                done(null, false);
-            }
-        });
+
+    passport.use(new JwtStrategy(opts, function (jwt_payload, done) {
+        // need to know how does findone behaves it seems to be really buggy
+        // implement promise instead of call back
+        User.findOne({_id: jwt_payload._doc._id}).exec()
+            .then(function (user) {
+                if (user) {
+                    done(null, user);
+                } else {
+                    done(null, false);
+                }
+            })
+            .catch(function(err){
+                if (err) {
+                    return done(err, false);
+                }
+            });
     }));
 };
