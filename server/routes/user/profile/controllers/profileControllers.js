33a64f34857aebff37f98ea17897b3464256ec5f
Kai Huang <huangkai2518@gmail.com>
2017-05-12 14:47:25 -0700

    done the functions about user profile; leave some bugs in notes

--- server/routes/user/profile/controllers/profileControllers.js
+++ server/routes/user/profile/controllers/profileControllers.js
@@ -6,36 +6,47 @@ const jwtDecode = require('jwt-decode');
 
 module.exports = {
     getUserProfileController: function(req, res, next) {
-        let email = getEmailFromToken (req);
-
-        if (email !== null){
-            res.send(email);
-        }
-        else{
-            res.json({
-                success: false,
-                message: "Unable get email from token",
-            });
-        }
+        res.json({
+            name : req.user.name,
+            email: req.user.email,
+            createAt: req.user.createdAt,
+            lastModified: req.user.lastModifiedTime,
+        });
     },
 
     updateUserProfileController: function(req, res, next) {
-        res.send("updated user profile");
+        // let keys = Object.keys(req.body);
+        let user    = req.user;
+        let success = true;
+        let message = 'User profile is successfully updated';
+
+        if (req.body.name != null){
+            user.name = req.body.name;
+            user.save(function(err){
+                let errMessage = 'User name is unable to update';
+                if (err) {
+                    success = false;
+                    message = errMessage;
+                }
+            });
+        }
+
+        if (req.body.image != null){
+            user.image = req.body.image;
+            user.save(function(err){
+                let errMessage = 'User image is unable to update';
+                if (err){
+                    message = (success === false) ? (message + " and " + errMessage) : (errMessage);
+                    success = false;
+                }
+            });
+        }
+
+        res.json({
+            success: success,
+            message: message,
+        });
+
     },
 };
 
-
-function getEmailFromToken (req){
-    let token = req.headers.authorization;
-
-    if (token) {
-        let payload = jwtDecode(token);
-        if (payload.hasOwnProperty('email')){
-            return payload.email;
-        }
-    }
-
-    return null;
-
-
-}
