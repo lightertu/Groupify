b5875ae56b8fad4ab595be21ad46dc41e06eebca
lightertu <ruitu.login@gmail.com>
2017-05-16 12:25:30 -0700

    fixing some bugs

--- server/routes/activities/participants/controllers/deleteParticipantController.js
+++ server/routes/activities/participants/controllers/deleteParticipantController.js
@@ -7,9 +7,13 @@ const Participant = require("../../../../models/").Participant;
 
 module.exports = function (req, res, next) {
     Participant.findOneAndUpdate(
-        {_id: req.params.participantId, _activity: req.params.activityId, _creator: req.user._id, isDeleted: false},
         {
-            // TODO: more changes can be added
+            _id: req.params.participantId,
+            _activity: req.params.activityId,
+            _creator: req.user._id,
+            isDeleted: false
+        },
+        {
             $set: {
                 "lastModifiedTime": Date.now(),
                 "isDeleted": true,
@@ -28,15 +32,28 @@ module.exports = function (req, res, next) {
             })
         }
 
-        return res.json({
-            success: true,
-            message: "successfully deleted: " + participant.name
-        })
+        /* remove this item from User.activities array */
+        Activity.findOneAndUpdate(
+            { _id: participant._activity },
+            { $pull: { 'participants': participant._id } }
+        ).exec().then(function(activity){
+            return res.json({
+                success: true,
+                activity: activity
+            });
+        }).catch(function(err) {
+            // TODO: set http header to resource not found
+            console.log(err);
+            return res.json({
+                success: false,
+                message: err,
+            });
+        });
     })
 
     .catch(function (err) {
         // TODO: set http status code system error
-        console.log(err)
+        console.log(err);
         return res.json({
             success: false,
             error: err,
