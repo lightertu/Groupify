edba003d78ccc58849c7a7f82ee4f2dfcd10d877
lightertu <ruitu.login@gmail.com>
2017-05-15 18:12:16 -0700

    post activity is working

--- server/models/User.js
+++ server/models/User.js
@@ -28,8 +28,9 @@ let UserSchema = Schema({
         required: true,
     },
 
+    // automatically sort this array based on its last modified data
     activities: {
-        type: [{ type: Schema.ObjectId, ref: "Activity"}],
+        type: [{type: Schema.ObjectId, ref: "Activity"}],
     },
 
     // every model has this
@@ -52,24 +53,23 @@ let UserSchema = Schema({
     }
 });
 
-
 /* hash password before user saves the password */
-UserSchema.pre('save', function(next) {
+UserSchema.pre('save', function (next) {
     let user = this;
 
     // update time
-    if (!this.isNew){
+    if (!this.isNew) {
         user.lastModifiedTime = Date.now();
     }
 
     // update hashed password if needed
     if (this.isModified('password') || this.isNew) {
-        bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.genSalt(10, function (err, salt) {
             if (err) {
                 return next(err);
             }
 
-            bcrypt.hash(user.password, salt, function(err, hash) {
+            bcrypt.hash(user.password, salt, function (err, hash) {
                 if (err) {
                     return next(err);
                 }
@@ -79,15 +79,15 @@ UserSchema.pre('save', function(next) {
             });
         });
     }
-    else{
+    else {
         return next();
     }
 });
 
 
 /* compare password if user login */
-UserSchema.methods.comparePassword = function(password, callback) {
-    bcrypt.compare(password, this.password, function(err, isMatch) {
+UserSchema.methods.comparePassword = function (password, callback) {
+    bcrypt.compare(password, this.password, function (err, isMatch) {
         if (err) {
             return callback(err);
         }
