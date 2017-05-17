15a500a72a2649c6f36b82e31fb7005a4316601d
Lightertu <ruit@uoregon.edu>
2017-05-17 11:21:26 -0700

    implemented both front end and backend routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,10 +2,18 @@
  * Created by rui on 5/9/17.
  */
 const express = require('express')
-    , router = express.Router();
+    , router = express.Router()
+    , createErrorHandler = require('./utils').createErrorHandler
+    , HttpStatus = require('http-status-codes');
 
 router.use('/auth', require('./auth'));
 router.use('/activities', require('./activities'));
 router.use('/user', require('./user'));
+
+router.use(function(req, res, next) {
+    if (!req.route)
+        return createErrorHandler(res, HttpStatus.NOT_FOUND)("404");
+    next();
+});
 
 module.exports = router;
