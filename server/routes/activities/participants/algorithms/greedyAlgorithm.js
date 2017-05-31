bba3857ff64348202d689f617bb3c89934010930
Kai Huang <huangkai2518@gmail.com>
2017-05-31 03:00:12 -0700

    greedyAlgorithm rebuild

--- server/routes/activities/participants/algorithms/greedyAlgorithm.js
+++ server/routes/activities/participants/algorithms/greedyAlgorithm.js
@@ -5,272 +5,336 @@ Email: huangkai2518@gmail.com
 
 */
 
-const Set = require("immutable");
+const Set = require("immutable").Set;
+const mergeSort = require("./mergeSort");
 
-
-function msSortTwoList(l1, l2){
-	let lgth1 = l1.length;
-	let lgth2 = l2.length;
-	let lgth;
-	let i=0, j=0, k=0;
-	let resList = [], l=[];
-	while (i<lgth1 && j<lgth2){
-		if (l1[i].num < l2[j].num){
-			resList.push(l1[i]);
-			i++;
-		}
-		else{
-			resList.push(l2[j]);
-			j++;
-		}
-	}
-	if (i === lgth1){
-		k = j; lgth = lgth2; l = l2;
-	}
-	else{
-		k = i; lgth = lgth1; l = l1;
-	}
-	for(; k<lgth; k++){
-		resList.push(l[k]);
-	}
-	return resList;
-}
-
-function mergeSortSplit(list){
-	let lgth = list.length;
-	let l1 = [], l2 = [];
-	let i=0;
-	for(; i<Math.floor(lgth/2); i++){
-		l1.push(list[i]);
-	}
-	for (; i<lgth; i++){
-		l2.push(list[i]);
-	}
-	return [l1,l2];
-}
-
-function mergeSort(list){
-	if (list.length < 2){
-		return list;
-	}
-	list = mergeSortSplit(list);
-	let l1 = mergeSort(list[0]);
-	let l2 = mergeSort(list[1]);
-	return msSortTwoList(l1, l2);
-}
-
-function contain(list, value){
-	let sum = 0;
-	let length = list.length;
-	for (let i=0; i<length; i++){
-		if (list[i] === value){
-            sum++;
-		}
-	}
-	return sum;
-}
-
-function min(list, ilist){
-	let lgth = list.length;
-	let min  = Number.MAX_SAFE_INTEGER;
-	let index = 0;
-	for (let i=0; i<lgth; i++){
-		if (list[i] !== 0 && contain(ilist,i) === 0){
-			if (min > list[i]){
-				min = list[i];
-				index = i;
-			}
-		}
-	}
-	return index;
+function compareNumOfWorkObject(work1, work2){
+    return work1.num < work2.num;
 }
 
 
-// get best partners with its position in sortL
-function minPartners(list, sortL, potn, size){
-	let lgth = potn.length;
-	let l = [], res = [];
-	for (let i=0; i<lgth; i++){
-		if (potn[i] !== 0){
-			l.push(list[i]);
-		}
-	}
-	l = mergeSort(l);
-	for (let j=0; j<size-1; j++){
-		res.push(l[j].ord);
-	}
-	return res;
+//
+// function contain(list, value){
+// 	let sum = 0;
+// 	let length = list.length;
+// 	for (let i=0; i<length; i++){
+// 		if (list[i] === value){
+//             sum++;
+// 		}
+// 	}
+// 	return sum;
+// }
+
+// function min(list, ilist){
+// 	let lgth = list.length;
+// 	let min  = Number.MAX_SAFE_INTEGER;
+// 	let index = 0;
+// 	for (let i=0; i<lgth; i++){
+// 		if (list[i] !== 0 && contain(ilist,i) === 0){
+// 			if (min > list[i]){
+// 				min = list[i];
+// 				index = i;
+// 			}
+// 		}
+// 	}
+// 	return index;
+// }
+
+
+// // get best partners with its position in sortL
+// function minPartners(list, potn, size){
+// 	let lgth = potn.length;
+// 	let l = [], res = [];
+// 	for (let i=0; i<lgth; i++){
+// 		if (potn[i] !== 0){
+// 			l.push(list[i]);
+// 		}
+// 	}
+// 	l = mergeSort(l, compareNumOfWorkObject);
+// 	for (let j=0; j<size-1; j++){
+// 		res.push(l[j].ord);
+// 	}
+// 	return res;
+// }
+
+
+function getOptimalGroupPartnersOfTheFirstOne(firstOne, notFinishedWObjs, groupSize){
+    let partnersIndexes = [], removeList = [];
+
+    let count = groupSize -1 , i = 1;
+    while (count !== 0 && i < notFinishedWObjs.length){
+        const index = notFinishedWObjs[i].originIndex;
+
+        if (firstOne.potn[index] !== 0){
+            partnersIndexes.push(index);
+            removeList.push(i);
+            count--;
+        }
+        i++;
+    }
+
+    // remove element in notFinishedWObjs
+    let m = 0;
+    removeList.forEach(function (index) {
+        notFinishedWObjs.splice(index - m, 1);
+        m++;
+    })
+
+    return partnersIndexes;
 }
 
 //min, max should be integers. Sample input: min=3, max=5
-function getRandomNum(min, max){
-	return Math.floor(Math.random() * (max-min+1)) + min;
+function generateRandomNumber(min, max){
+    return Math.floor(Math.random() * (max-min+1)) + min;
 }
 
 
-function addRestStd(teams, sortL, unavlStd, size){
-	for (let ii=0; ii<sortL.length; ii++){
-		unavlStd.push(sortL[ii].index);
-	}
-	let num = Math.floor(unavlStd.length/size);
-	let r   = unavlStd.length%size;
-	for (let i=0; i<num; i++){
-		let lst = [];
-		for (let j=0; j<size; j++){
-			lst.push(unavlStd[i*size+j]);
-		}
-		teams.push(lst);
-	}
-	// let lr = [];
-	for (let k=0; k<r; k++){
-		let rdmNum = getRandomNum(0,teams.length-1);
-		teams[rdmNum].push(unavlStd[i+k]);
-		// lr.push(unavlStd[i+k]);
-	}
-	// teams.push(lr);
-	return teams;
+function groupTheRests(allGroups, rests, groupSize){
+    let numberOfRestGroups = Math.floor(rests.length/groupSize);
+    let numberOfRemains = rests.length % groupSize;
+
+    let i;
+    for (i=0; i<numberOfRestGroups; i++){
+        let group = [];
+        for (let j=0; j<groupSize; j++){
+            group.push(rests[i * groupSize + j])
+        }
+        allGroups.push(group);
+    }
+    // console.log(allGroups);
+    // console.log(rests);
+    for (let k=0; k<numberOfRemains; k++){
+        let randomNumber = generateRandomNumber(0, allGroups.length - 1);
+        // console.log(randomNumber + "  " + k + " " + allGroups[randomNumber] + " " + i);
+        allGroups[randomNumber].push(rests[i + k].originIndex);
+    }
+// console.log("fdsafdsfsasdsd");
+    return allGroups;
 }
 
-function order(sortL, list){
-	let lgth = sortL.length;
-	for (let i=0; i<lgth; i++){
-		sortL[i].ord = i;
-	}
-	return list;
+
+function generateGroupsForEveryWorkObject (sortedWList, groupSize, successRate){
+    let allGroups = [], rests = [], notFinishedWObjs = [];
+// console.log("abc1");
+    for (let i=0; i<sortedWList.length; i++){
+        notFinishedWObjs.push({
+            sortIndex: i,
+            originIndex: sortedWList[i].index,
+        });
+    }
+    // console.log("abc2");
+    while (notFinishedWObjs.length >= groupSize){
+        const firstOne = sortedWList[notFinishedWObjs[0].sortIndex];
+
+        if (firstOne.num < groupSize - 1){
+            rests.push(firstOne.index);
+        }
+        else{
+            const partnersIndexes = getOptimalGroupPartnersOfTheFirstOne(firstOne, notFinishedWObjs, groupSize);
+
+            let group = [firstOne.index];
+            group = group.concat(partnersIndexes);
+            allGroups.push(group);
+
+            successRate++;
+        }
+
+        notFinishedWObjs.splice(0, 1);
+    }
+    // console.log("abc3");
+    for (let j=0; j<notFinishedWObjs.length; j++){
+        rests.push(notFinishedWObjs[j]);
+    }
+    // console.log("abc4");
+    return groupTheRests(allGroups, rests, groupSize);
 }
 
-function removePotential(list, sortL, index){
-	for (let k=0; k<sortL.length; k++){
-		sortL[k].potn[index] = 0;
-	}
-	for (let m=0; m<list.length; m++){
-		list[m].potn[index] = 0;
-	}
-}
 
-function removeStd(pos, sortL){
-	for (let i=0; i<sortL.length; i++){
-		if (sortL[i].ord === pos ){
-			sortL.splice(i ,1);
-			return;
-		}
-	}
-}
+// //min, max should be integers. Sample input: min=3, max=5
+// function getRandomNum(min, max){
+// 	return Math.floor(Math.random() * (max-min+1)) + min;
+// }
+//
+//
+// function addRestStd(teams, sortL, unavlStd, size){
+// 	for (let ii=0; ii<sortL.length; ii++){
+// 		unavlStd.push(sortL[ii].index);
+// 	}
+// 	let num = Math.floor(unavlStd.length/size);
+// 	let r   = unavlStd.length%size;
+// 	for (let i=0; i<num; i++){
+// 		let lst = [];
+// 		for (let j=0; j<size; j++){
+// 			lst.push(unavlStd[i*size+j]);
+// 		}
+// 		teams.push(lst);
+// 	}
+// 	// let lr = [];
+// 	for (let k=0; k<r; k++){
+// 		let rdmNum = getRandomNum(0,teams.length-1);
+// 		teams[rdmNum].push(unavlStd[i+k]);
+// 		// lr.push(unavlStd[i+k]);
+// 	}
+// 	// teams.push(lr);
+// 	return teams;
+// }
+//
+// function order(sortL, list){
+// 	let lgth = sortL.length;
+// 	for (let i=0; i<lgth; i++){
+// 		sortL[i].ord = i;
+// 	}
+// 	return list;
+// }
+//
+// function removePotential(list, sortL, index){
+// 	for (let k=0; k<sortL.length; k++){
+// 		sortL[k].potn[index] = 0;
+// 	}
+// 	for (let m=0; m<list.length; m++){
+// 		list[m].potn[index] = 0;
+// 	}
+// }
+//
+// function removeStd(pos, sortL){
+// 	for (let i=0; i<sortL.length; i++){
+// 		if (sortL[i].ord === pos ){
+// 			sortL.splice(i ,1);
+// 			return;
+// 		}
+// 	}
+// }
 
-function match(list, sortL, size){
-	let unavlStd = [];
-	let teams 	 = [];
+// function match(list, sortL, size){
+// 	let unavlStd = [];
+// 	let teams 	 = [];
+//
+// 	while(sortL.length >= size){
+// 		let partners = [];
+// 		let team 	 = [];
+// 		if (sortL[0].num < size - 1){
+// 			unavlStd.push(sortL[0].index);
+// 		}
+// 		else{
+// 			partners = minPartners(list,  sortL[0].potn, size);
+//
+// 			for (let i=0; i<partners.length; i++){
+// 				let pos = partners[i];
+// 				team.push(sortL[pos].index);
+// 				removePotential(list, sortL, sortL[pos].index);
+// 			}
+// 			for (let j=0; j<partners.length; j++){
+// 				removeStd(partners[j], sortL);
+// 			}
+// 			team.push(sortL[0].index);
+// 			teams.push(team);
+// 		}
+// 		removePotential(list, sortL, sortL[0].index);
+// 		sortL.splice(0,1);
+// 		order(sortL, list);
+// 	}
+// 	addRestStd(teams, sortL, unavlStd, size);
+// 	return teams;
+// }
 
-	while(sortL.length >= size){
-		let partners = [];
-		let team 	 = [];
-		if (sortL[0].num < size - 1){
-			unavlStd.push(sortL[0].index);
-		}
-		else{
-			partners = minPartners(list, sortL, sortL[0].potn, size);
+function groupingPars(groups, pars){
+	for (let i=0; i<groups.length; i++){
 
-			for (let i=0; i<partners.length; i++){
-				let pos = partners[i];
-				team.push(sortL[pos].index);
-				removePotential(list, sortL, sortL[pos].index);
-			}
-			for (let j=0; j<partners.length; j++){
-				removeStd(partners[j], sortL);
-			}
-			team.push(sortL[0].index);
-			teams.push(team);
-		}
-		removePotential(list, sortL, sortL[0].index);
-		sortL.splice(0,1);
-		order(sortL, list);
-	}
-	addRestStd(teams, sortL, unavlStd, size);
-	return teams;
-}
-
-function grouping(teams, pars, size){
-	let lgth = teams.length;
-	for (let i=0; i<lgth; i++){
-		for (let j=0; j<teams[i].length; j++){
-			let index = teams[i][j];
+		for (let j=0; j<groups[i].length; j++){
+			let index = groups[i][j];
 			pars[index].groupNumber = i;
 		}
 	}
 	return pars;
 }
 
-function successRate(teams, pars){
-	let total = teams.length;
-	let success = 0;
-	for (let i=0; i<teams.length; i++) {
-		for (let ii=0; ii<30; ii++){ // 30 is the number of time slice
-			let sum = 0;
-			for (let j=0; j<teams[i].length; j++){
-				let index = teams[i][j];
-				sum += pars[index].meetingTimes[ii];
-			}
-			if (sum === teams[i].length){
-				success++;
-				break;
-			}
-		}
-	}
-	return success/total;
-}
+// function successRate(groups, pars){
+// 	let total = groups.length;
+// 	let success = 0;
+//     let allAnswers = Set([]);
+//     pars.forEach(function (par){
+//         par.surveyResponses.forEach(function (response) {
+//             response.answer.forEach(function (ans){
+//                 allAnswers = allAnswers.add(ans);
+//             });
+//         });
+//     });
+//
+// 	for (let i=0; i<groups.length; i++) {
+// 		for (let ii=0; ii<allAnswers.size; ii++){
+// 			let sum = 0;
+// 			for (let j=0; j<groups[i].length; j++){
+// 				let index = groups[i][j];
+// 				sum += pars[index].meetingTimes[ii];
+// 			}
+// 			if (sum === groups[i].length){
+// 				success++;
+// 				break;
+// 			}
+// 		}
+// 	}
+// 	return success/total;
+// }
+
 
 function validateInput(pars, size){
     let result = Array.isArray(pars);
-    pars.forEach(function (p) {
-        result = result
-            && p.hasOwnProperty('_id')
-            && p.hasOwnProperty('groupNumber');
-        if (result){
-            result = result && ObjectIdIsValid(p._id)
-                && typeof p.groupNumber === 'number'
-                && p.groupNumber % 1 === 0;
-        }
-    });
-
-    return result
-        && typeof size !== 'number'
-        && size > 0
-        && size % 1 === 0;
+    if (result){
+        pars.forEach(function (p) {
+            result = result && p.toObject().hasOwnProperty('groupNumber');
+            if (result){
+                result = result && Number.isInteger(p.groupNumber);
+            }
+        });
+        return result
+            && Number.isInteger(size)
+            && size > 0;
+    }
 }
 
-function greedyAlgorithm(pars, size){
-    if (!validateInput(pars, size)){
+function greedyAlgorithm(pars, groupSize){
+    if (!validateInput(pars, groupSize)){
         return [];
     }
 
+    // console.log("111111111111111here");
+
+    let successRate = 0;
+
     let nestedBoolArray = createNestedBooleanArrayForAllPars(pars);
 
+    // console.log("222222222222222here");
     let workList = createWorkList(nestedBoolArray, booleanArrayCompare);
 
-    let sortedWorkList = mergeSort(workList);
+    // console.log(workList);
+    let sortedWList = mergeSort(workList, compareNumOfWorkObject);
 
-    workList = order(sortedWorkList, workList);
+    // console.log("444444444444444here");
+    // workList = order(sortedWList, workList);
 
-    let teams = match(workList, sortedWorkList, size);
+    // console.log("555555555555555here");
+    // let teams = match(workList, sortedWList, groupSize);
+    let groups = generateGroupsForEveryWorkObject(sortedWList, groupSize, successRate);
 
-    grouping(teams, pars, size);
+    // console.log("666666666666666here");
+    groupingPars(groups, pars);
 
-    return successRate(teams, pars);
+    // console.log("111111111111111here");
+    successRate = successRate/groups.length;
+    return successRate;
 
 }
 
 module.exports = greedyAlgorithm;
 
 
-function WorkObject (index, num, ord, potn) {
+function WorkObject (index, num, potn) {
     this.index 	= index;
     this.num   	= num;  // number of potential partners
-    this.ord    = ord;  // the order of the students, sort by their match posibility
     this.potn 	= potn; // potential partners
 }
 
 function createNestedBooleanArrayForAllPars(pars){
-
     let allAnswers = Set([]);
     let nestedArray = [];
 
@@ -281,7 +345,6 @@ function createNestedBooleanArrayForAllPars(pars){
             });
         });
     });
-
 
     pars.forEach(function (par) {
         let booleanArray = [];
@@ -300,7 +363,7 @@ function createNestedBooleanArrayForAllPars(pars){
 
 
 // input: [ [T, T], [T, F] ]; output: [ WorkObject1, WorkObject2]
-function createWorkList(nestedArray, compareFcn){
+function createWorkList(nestedArray){
     let inputLength = nestedArray.length;
     let workList = [];
 
@@ -308,7 +371,7 @@ function createWorkList(nestedArray, compareFcn){
         let similarCount = 0, similarity = [];
         for (let j=0; j<inputLength; j++){
             if (i !== j){
-                let similarValue = compareFcn(nestedArray[i], nestedArray[j]);
+                let similarValue = booleanArrayCompare(nestedArray[i], nestedArray[j]);
                 similarCount += (similarValue === 0) ? 0 : 1;
                 similarity.push(similarValue);
             }
@@ -316,7 +379,7 @@ function createWorkList(nestedArray, compareFcn){
                 similarity.push(0);
             }
         }
-        let workObject = new WorkObject(i, similarCount, -1, similarity);
+        let workObject = new WorkObject(i, similarCount, similarity);
         workList.push(workObject);
     }
     return workList;
---
+++ server/routes/activities/participants/algorithms/mergeSort.js
@@ -0,0 +1,56 @@
+function mergeSort(list){
+    if (list.length < 2){
+        return list;
+    }
+    list = splitSingleListToTwoLists(list);
+    let list1 = mergeSort(list[0]);
+    let list2 = mergeSort(list[1]);
+    return sortTwoListToSingleList(list1, list2);
+}
+
+function sortTwoListToSingleList(list1, list2){
+    let i=0, j=0, k=0;
+    let length1 = list1.length, length2 = list2.length, tempLgth;
+    let resultLst = [], rests = [];
+
+    while (i < length1 && j < length2){
+        if (compareFcn(list1[i], list2[j])){
+            resultLst.push(list1[i]);
+            i++;
+        }
+        else{
+            resultLst.push(list2[j]);
+            j++;
+        }
+    }
+    if (i === length1){
+        k = j; tempLgth = length2; rests = list2;
+    }
+    else{
+        k = i; tempLgth = length1; rests = list1;
+    }
+    for(; k<tempLgth; k++){
+        resultLst.push(rests[k]);
+    }
+    return resultLst;
+}
+
+function splitSingleListToTwoLists(list){
+    let length = list.length;
+    let list1 = [], list2 = [];
+    let i=0;
+    for(; i<Math.floor(length/2); i++){
+        list1.push(list[i]);
+    }
+    for (; i<length; i++){
+        list2.push(list[i]);
+    }
+    return [list1,list2];
+}
+
+function compareFcn(work1, work2){
+    return work1.num < work2.num;
+}
+
+module.exports = mergeSort;
+
