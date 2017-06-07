918ce9e0eb81ee388eac7bb689b2b1a0e5617cc9
Kai Huang <huangkai2518@gmail.com>
2017-06-07 14:27:16 -0700

    fixing the sendGrid problem

--- server/config/main.js
+++ server/config/main.js
@@ -8,5 +8,5 @@ module.exports = {
     'secret': 'asdawldjal',
     databaseUrl: 'mongodb://teamdivider:CIS4222017@ds137101.mlab.com:37101/teamdivider',
     authenticationMiddleware: passport.authenticate('jwt', { session: false }),
-    SENDGRID_API_KEY: "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",
+    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
 };
